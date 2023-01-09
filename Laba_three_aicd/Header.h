e064125f6c392ab057764c131e5f92e00d81e308
Colnuwko <Vladislav290403@gmail.com>
2023-01-09 04:20:09 +0400

    Добавьте файлы проекта.

---
+++ Laba_three_aicd/Header.h
@@ -0,0 +1,496 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
+#include <cmath>
+#include <complex>
+#include <stdexcept>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class Vector {
+private:
+	vector<T> data;
+	int _size;
+	T epsilon;
+public:
+
+	Vector() { //конструктор по умолчаниюю
+		srand(time(0));
+		_size = rand() % 3 + 10;
+		epsilon = 0, 000001;
+		data = vector<T> (_size);
+		for (size_t i = 0; i < _size; i++)
+		{
+			data.at(i) = ((T)rand()) / 300 - 100;
+		}
+
+	}
+
+	Vector(int size, T eps) { // конструктор с параметром
+		if (size < 1) throw invalid_argument("Недопустимое значение");
+		_size = size;
+		epsilon = abs(eps);
+		data = vector<T>(_size);
+		cout << "Введите значение:\n";
+		long double a;
+		for (size_t i = 0; i < _size; i++)
+		{
+			cout << "A[" << i << "] = ";
+			cin >> a;
+			data.at(i) = (T)a;
+		}
+	}
+	Vector(const Vector& a) = default;
+	//  { // конструктор копирования
+	//	_size = a._size;
+	//	data = new T[_size];
+	//	epsilon = a.epsilon;
+	//	for (size_t i = 0; i < _size; i++)
+	//	{
+	//		data[i] = a.data[i];
+	//	}
+	// }
+	~Vector() = default;
+	//void Vprint() // ìåòîä âûâîäà
+	//{
+	//	for (size_t i = 0; i < _size; i++)
+	//	{
+	//		cout << "A[" << i << "] = " << data[i];
+	//	}
+	//	cout << "/n";
+	//}
+	int Get_size() { return _size; }
+	/*	double operator()(int i) const {
+			if (i<0 or i>_size) throw "Invalid index";
+			return data[i];
+		}
+		double& operator()(int i) {
+			if (i<0 or i>_size)  throw "Invalid index";
+			return data[i];
+		}
+	*/
+	T operator[](int i) const { //×Òåíèå è çàïèñü
+		if (i<0 or i>_size - 1) throw out_of_range("Недопустимое значение");
+		return data[i];
+	}
+
+	T& operator[](int i) {
+		if (i<0 or i>_size - 1)  throw out_of_range("Недопустимое значение");
+		return data[i];
+	}
+	friend ostream& operator<<(ostream& os, const Vector& a)
+	{
+		/*for (int i = 0; i < a._size; ++i)
+		{
+			os << left << a.data[i] << ' ';
+		}
+		return os;*/
+	/*	for (auto it = a.data.begin(); it != a.data.end(); ++it)
+		{
+			os << left << *it << ' ';
+		}*/
+		for (const auto& n : a.data) {
+			os << n << " ";
+		}
+		return os;
+	}
+	bool operator==(const Vector& a) {
+		if (_size != a._size) {
+			return false;
+		}
+		else {
+			for (size_t i = 0; i < _size; i++)
+			{
+				if (fabs(data[i] - a.data[i]) > epsilon) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	bool operator!=(const Vector& a) {
+		return !(*this == a);
+	}
+
+	Vector& operator=(const Vector& a) = default;
+	//{
+	//	//Vector<T> temp;
+	//	_size = a._size;
+	//	epsilon = a.epsilon;
+	//	for (size_t i = 0; i < a._size; i++)
+	//	{
+	//		data[i] = a.data[i];
+	//	}
+	//	return *this;
+	//}
+	Vector& operator+=(const Vector& a) {		//сложение векторов
+		if (_size != a._size) throw logic_error("Невозможжно сложить векторы разной длины");
+		for (size_t i = 0; i < _size; i++)
+		{
+			data[i] += a.data[i];
+		}
+		return *this;
+	}
+	Vector operator+(const Vector& a) const { // *this + a 
+		Vector temp(*this);
+		temp += a;
+		//cout << temp << endl;
+		Vector tmp(temp);
+		return tmp;
+	}
+	Vector operator-=(const Vector a) {
+		if (_size != a._size) throw logic_error("Невозможжно вычесть векторы разной длины");
+		for (size_t i = 0; i < _size; i++)
+		{
+			data[i] -= a.data[i];
+		}
+		return *this;
+	}
+	Vector operator-(const Vector& a) const { //вычитание векторов
+		Vector temp(*this);
+		temp -= a;
+		//cout << temp << endl;
+		return temp;
+	}
+	Vector& operator*=(const Vector& a) { //*this это наш переданный темп, а а соотвтвествеенно как аргумент а
+		if (_size != a._size) throw logic_error("Невозможжно умн векторы разной длины");
+		for (size_t i = 0; i < _size; i++)
+		{
+			data[i] *= a.data[i];
+		}
+		return *this;
+	}
+	T operator*(const Vector& a) const { // скалярное
+		Vector<T> temp(*this);
+		temp *= a;
+		//cout << temp << endl;
+		T scalar = 0;
+		for (const auto& n : a.data) 
+		{
+			scalar += n;
+		}
+		return scalar;
+	}
+	Vector& operator*=(const T a) { // Вектор на число
+		for (auto& n : data)
+		{
+			n *= a;
+		}
+		return *this;
+	}
+	Vector operator*(const T a) const { // 
+		Vector temp(*this);
+		temp *= a;
+		//cout << temp << endl;
+		return temp;
+	}
+	friend Vector operator*(const T b, const Vector& a) { // число на вектор
+		Vector temp(a);
+		temp *= b;
+		//cout << temp << endl;
+		return temp;
+	}
+
+	/*Vector& operator/=(const Vector& a) { //*this ýòî íàø ïåðåäàííûé òåìï, à à ñîîòâòâåñòâååííî êàê àðãóìåíò à
+		if (_size != a._size) throw "Íåâîçìîææíî ðàçäåëèòü âåêòîðû";
+		for (size_t i = 0; i < _size; i++)
+		{
+			if (a.data[i] == 0) throw "íåëüçÿ äåëèòü íà íîëü";
+			data[i] /= a.data[i];
+		}
+		return *this;
+	}
+	Vector operator/(const Vector& a) const { // *this + a
+		Vector temp(*this);
+		temp /= a;
+		//cout << temp << endl;
+		return temp;
+	}
+	*/
+	Vector& operator/=(const T a) {
+		if (a == 0) throw logic_error("Невозможно делит 0");
+		for (auto& n : data)
+		{
+			n /= a;
+		}
+		return *this;
+	}
+	Vector operator/(const T a) const { // *this + a 
+		Vector temp(*this);
+		temp /= a;
+		//cout << temp << endl;
+		return temp;
+	}
+	static T square(Vector& a, Vector& b)// âû÷èñëåíèå ïëîùàäè òðåóãîëüíèêà ÷åðçå 2 âåêòîðà
+	{
+		return 0.5 * sin(a, b) * length(a, b);
+	}
+	static T sin(Vector a, Vector b) //âû÷åñëåíèå ñèíóñà äëÿ ïëîùàäè
+	{
+		if (a._size != b._size) throw logic_error("Íåâîçìîææíî âû÷èñëèòü ñèíóñ ìåæäó âåêòîðàìè ðàçíîé ðàçìåðíîñòè");
+		T chislitel = 0;
+		T temp = 0, tmp = 0;
+		T znamenatel = 1;
+		//for (size_t i = 0; i < a._size; i++)
+		//{
+		chislitel = a * b;
+
+		//}
+		for (size_t i = 0; i < a._size; i++)
+		{
+			temp += pow(a.data[i], 2);
+			tmp += pow(b.data[i], 2);
+		}
+		temp = sqrt(temp);
+		tmp = sqrt(tmp);
+		znamenatel = temp * tmp;
+		return sqrt(1 - pow((chislitel / znamenatel), 2));
+	}
+	static T length(Vector a, Vector b) //âû÷åñëåíèå ïðîèçâåäåíèÿ äëèí âåêòîðîâ
+	{
+		T temp = 0;
+		T tmp = 0;
+		for (size_t i = 0; i < a._size; i++)
+		{
+			temp += pow(a.data[i], 2);
+			tmp += pow(b.data[i], 2);
+		}
+		temp = sqrt(temp);
+		tmp = sqrt(tmp);
+		return temp * tmp;
+	}
+};
+template <typename V>
+class Vector <complex<V>>
+{
+private:
+	vector<complex<V>> data;
+	int _size;
+	complex<V> epsilon;
+public:
+	Vector() { //êîíñòðóêòîð ïî óìîë÷àíèþþ
+		srand(time(0));
+		_size = rand() % 3 + 10;
+		epsilon = complex<V>(0.0001, 0.00001);
+		data = vector<complex<V>>(_size);
+		for (size_t i = 0; i < _size; i++)
+		{
+			data.at(i) = complex<V>((V)rand() / 3, (V)rand() / 4);
+		}
+
+	}
+
+	Vector(int size, complex<V> eps) { // êîíñòðóêòîð ñ ïàðàìåòðîì
+		if (size < 1) throw invalid_argument("Íåäîïóñòèìîå çíà÷åíèå");
+		_size = size;
+		data = vector<complex<V>>(size);
+		cout <<"ВВедите точность" << "Введите значения в виде (число,число):\n";
+		epsilon = eps;
+		for (size_t i = 0; i < size; i++)
+		{
+			cin >> data[i];
+		}
+	}
+	Vector(const Vector<complex<V>>& a) = default;
+	
+	~Vector() = default;
+	friend ostream& operator<<(ostream& os, const Vector<complex<V>>& a) //âûâîä
+	{
+		for (const auto& n : a.data) {
+			os << n << " ";
+		}
+		return os;
+	}
+	friend ostream& operator>>(ostream& os, complex<V>& x)
+	{
+		V a;
+		V b;
+		cout << "ВВедите реально значение";
+		cin >> a;
+		cout << "Введите мнимое значение";
+		cin >> b;
+		x = complex<V>(a, b);
+
+	}
+	int Get_size() { return _size; }
+	complex<V> operator[](int i) const { //×Òåíèå è çàïèñü
+		if (i<0 or i>_size - 1) throw out_of_range("Invalid index");
+		return data[i];
+	}
+
+	complex<V>& operator[](int i) {
+		if (i<0 or i>_size - 1)  throw out_of_range("Invalid index");
+		return data[i];
+	}
+
+	bool operator==(const Vector<complex<V>>& a) {				//Îïåðàòîðû ñðàâíåíèÿ
+		if (_size != a._size) {
+			return false;
+		}
+		else {
+			for (size_t i = 0; i < _size; i++)
+			{
+				if ((data[i].real() - a.data[i].real()) > epsilon.real()) {
+
+					return false;
+				}
+				if ((data[i].imag() - a.data[i].imag()) > epsilon.imag()) {
+
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	Vector& operator=(const Vector& a) = default;
+	//{
+	//	_size = a._size;
+	//	for (size_t i = 0; i < _size; i++)
+	//	{
+	//		data[i] = a.data[i];
+	//	}
+	//	return *this;
+	//}
+
+	Vector<complex<V>>& operator+=(const Vector<complex<V>>& a) {		//Ñëîæåíèå âåêòîðîâ
+		if (_size != a._size) throw logic_error("Íåâîçìîææíî ñëîæèòü âåêòîðû ðàçíîé äëèíû");
+		for (size_t i = 0; i < _size; i++)
+		{
+			data[i] += a.data[i];
+		}
+		return *this;
+	}
+	Vector<complex<V>> operator+(const Vector<complex<V>>& a) const { // *this + a 
+		Vector temp(*this);
+		temp += a;
+		//cout << temp << endl;
+		Vector tmp(temp);
+		return tmp;
+	}
+	Vector<complex<V>>& operator-=(const Vector<complex<V>>& a) {
+		if (_size != a._size) throw logic_error("Íåâîçìîææíî âû÷åñòü âåêòîðû ðàçíîé äëèíû");
+		for (size_t i = 0; i < _size; i++)
+		{
+			data[i] -= a.data[i];
+		}
+		return *this;
+	}
+	Vector<complex<V>> operator-(const Vector<complex<V>>& a) const { //âû÷èòàíèå âåêòîðîâ
+		Vector temp(*this);
+		temp -= a;
+		//cout << temp << endl;
+		return temp;
+	}
+	Vector<complex<V>>& operator*=(const Vector<complex<V>>& a) { //*this ýòî íàø ïåðåäàííûé òåìï, à à ñîîòâòâåñòâååííî êàê àðãóìåíò à
+		if (_size != a._size) throw logic_error("Íåâîçìîææíî óìíîæèòü âåêòîðû ðàçíîé äëèíû");
+		for (size_t i = 0; i < _size; i++)
+		{
+			data[i] *= a.data[i];
+		}
+		return *this;
+	}
+	complex<V> operator*(const Vector<complex<V>>& a) const { // ñêàëÿðíîå óìíîæåíèå ÂÅÊÒÎÐÎÂ
+		Vector <complex<V>>temp(*this);
+		//cout << temp << endl;
+				//новое!!!!
+
+		Vector<complex<V>>tmp(a);
+		for (size_t i = 0; i < tmp.Get_size(); i++)
+		{
+			V real = tmp[i].real();
+			V imag = tmp[i].imag();
+			complex<V> copy(real, -imag);
+			//cout << copy;
+			tmp[i] = copy;
+		}
+		temp *= tmp;
+		complex<V>scalar(0, 0);
+		for (size_t i = 0; i < _size; i++)
+		{
+			scalar += temp.data[i];
+		}
+		return scalar;
+	}
+	Vector<complex<V>>& operator*=(const complex<V> a) { // ÂÅÊÒÎÐ íà ÷èñëî
+		for (auto& n : data)
+		{
+			n *= a;
+		}
+		return *this;
+	}
+	Vector<complex<V>> operator*(const complex<V> a) const { // 
+		Vector <complex<V>>temp(*this);
+		temp *= a;
+		//cout << temp << endl;
+		return temp;
+	}
+	friend Vector<complex<V>> operator*(const complex<V> b, const Vector<complex<V>>& a) { // ×èñëî íà âåêòîð
+		Vector <complex<V>>temp(a);
+		temp *= b;
+		//cout << temp << endl;
+		return temp;
+	}
+	Vector<complex<V>>& operator/=(const complex<V> a) { //*this ýòî íàø ïåðåäàííûé òåìï, à à ñîîòâòâåñòâååííî êàê àðãóìåíò à
+		if (a.real() == 0 && a.imag() == 0) throw "Íåâîçìîææíî ðàçäåëèòü âåêòîðû";
+		for (auto& n : data)
+		{
+			n /= a;
+		}
+		return *this;
+	}
+	Vector<complex<V>> operator/(const complex<V> a) const { // *this \ a 
+		if (a.real() == 0 && a.imag() == 0) throw logic_error("нельзя делить на (0,0)");
+
+		Vector<complex<V>> temp(*this);
+		temp /= a;
+		//cout << temp << endl;
+		return temp;
+	}
+	static V square(Vector<complex<V>>& a, Vector<complex<V>>& b)// âû÷èñëåíèå ïëîùàäè òðåóãîëüíèêà ÷åðçå 2 âåêòîðà
+	{
+		complex<V> x(0.5, 0);
+		x = x * sin(a, b) * length(a, b);
+		return x.real();
+	}
+	static complex<V> sin(Vector<complex<V>> a, Vector<complex<V>> b) //âû÷åñëåíèå ñèíóñà äëÿ ïëîùàäè
+	{
+		if (a._size != b._size) throw logic_error("Íåâîçìîææíî âû÷èñëèòü ñèíóñ ìåæäó âåêòîðàìè ðàçíîé ðàçìåðíîñòè");
+		complex<V> numerator(0, 0);
+		complex<V> temp(0, 0);
+		complex<V> tmp(0, 0);
+		complex<V> denominator(1, 0);
+		//for (size_t i = 0; i < a._size; i++)
+		//{
+		numerator = a * b;
+		//}
+		for (size_t i = 0; i < a._size; i++)
+		{
+			temp += pow(a.data[i], 2);
+			tmp += pow(b.data[i], 2);
+		}
+		temp = sqrt(temp);
+		tmp = sqrt(tmp);
+		denominator = temp * tmp;
+		complex<V> w(1, 0);
+		complex<V> q(2, 0);
+		complex<V> res = sqrt(w - pow(numerator / denominator, q));
+
+		return res.real();
+	}
+	static complex<V> length(Vector<complex<V>> a, Vector<complex<V>> b) //âû÷åñëåíèå ïðîèçâåäåíèÿ äëèí âåêòîðîâ
+	{
+		complex<V> temp(0, 0);
+		complex<V> tmp(0, 0);
+		for (size_t i = 0; i < a._size; i++)
+		{
+			temp += pow(a.data[i], 2);
+			tmp += pow(b.data[i], 2);
+		}
+		temp = sqrt(temp);
+		tmp = sqrt(tmp);
+		return temp * tmp;
+	}
+};
---
+++ Laba_three_aicd/Source.cpp
@@ -0,0 +1,684 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include "Header.h"
+
+
+using namespace std;
+void menu_2()
+{
+	system("cls");
+	cout << "1. Создать рандомный вектор" << endl;
+	cout << "2. Создать вектор с параметрами" << endl;
+	cout << "3. Создать два одинаковых вектора" << endl;
+
+
+}
+void menu()
+{
+	system("cls");
+	cout << "Выберите тип данных" << endl;
+	cout << "1. int" << endl;
+	cout << "2. double" << endl;
+	cout << "3. float" << endl;
+	cout << "4. complex<double>" << endl;
+	cout << "5. complex<float>" << endl;
+}
+void menu_3()
+{
+	system("cls");
+	cout << "1. Сложить векторы" << endl;
+	cout << "2. Вычесть векторы" << endl;
+	cout << "3. Найти скалярное произведение" << endl;
+	cout << "4. Разделить вектор на число" << endl;
+	cout << "5. Умножить вектор на число" << endl;
+	cout << "6. вычислить площадь треугольника" << endl;
+	cout << "7. Сравнить вектора" << endl;
+	cout << "8. Изменить элемент вектора" << endl;
+	cout << "9. Просмотреть элемент вектора" << endl;
+	cout << "esc -  выход" << endl;
+}
+template <class T>
+void Nomain()
+{
+	Vector <T>A;
+	Vector <T>B;
+	cout << "Хорошо продолжим.." << endl;
+	int key = 0;
+	while (key != 27)
+	{
+		menu_2();
+		key = _getch();
+		switch (key)
+		{
+		case 49:
+		{
+			Vector<T> b;
+			B = b;
+			cout << "Ваши рандомные вектора" << endl << "Вектор А: " << A << endl << "Вектор В: " << B << endl;
+
+			//goto random;
+			_getch();
+			key = 27;
+			break;
+		}
+		case 50:
+		{
+			system("cls");
+			cout << "Давайте заполгним наши векторы" << endl << "Введите размерность первого вектора" << endl;
+			int size_one;
+			int size_two;
+			double epsilon;
+			cin >> size_one;
+			cout << "А теперь введем размерность 2 вектора" << endl;
+			cin >> size_two;
+			cout << "Замечательно, а теперь введите точность сравнения" << endl;
+			cin >> epsilon;
+			cout << "Самое время заполнить вектора" << endl;
+			Vector <T>a(size_one, epsilon);
+			cout << endl << "Остался последний вектор" << endl;
+			Vector <T>b(size_two, epsilon);
+			A = a;
+			B = b;
+			cout << "Готово, вы справились с этим" << endl << "Вы только поглядите какие они красивые :)" << endl << "Вектор А: " << A << endl << "Вектор В: " << B << endl;
+			//goto Par;
+			_getch();
+			key = 27;
+			break;
+		}
+		case 51:
+		{
+			system("cls");
+			cout << "1. Создать два моих собственных вектора" << endl << "2. Создать два одинаковых и рандомных вектора" << endl;
+			key = _getch();
+			switch (key)
+			{
+			case 49:
+			{
+				int size;
+				int epsilon;
+				cout << "Давайте заполгним наши векторы" << endl << "Введите размерность векторов" << endl;
+				cin >> size;
+				cout << "Замечательно, а теперь введите точность сравнения" << endl;
+				cin >> epsilon;
+				cout << "Самое время заполнить вектора" << endl;
+				Vector <T>a(size, epsilon);
+				A = a;
+				B = a;
+				cout << "Готово, вы справились с этим" << endl << "Вы только поглядите какие они красивые :)" << endl << "Вектор А: " << A << endl << "Вектор В: " << B << endl;
+				//goto My_copy;
+				_getch();
+				key = 27;
+				break;
+			}
+			case 50:
+			{
+				B = A;
+				cout << "Ваши рандомные вектора" << endl << "Вектор А: " << A << endl << "Вектор В: " << B << endl;
+
+				//goto random_copy;
+				_getch();
+				key = 27;
+				break;
+			}
+			}
+			_getch();
+			break;
+		}
+		}
+
+	}
+	key = 0;
+	while (key != 27)
+	{
+		menu_3();
+		cout << "Вектор А: " << A << endl << "Вектор В: " << B << endl;
+		key = _getch();
+		Vector <T>res;
+		switch (key)
+		{
+		case 49:
+		{
+
+			res = A + B;
+			cout << "A + B = " << res << endl;
+			_getch();
+			break;
+		}
+		case 50:
+		{
+			while (key != 0) {
+				cout << "Шо вычитаем?" << endl << "1. A - B" << endl << "2. B - A" << endl;
+
+
+				key = _getch();
+				if (key == 49) { res = A - B; cout << "A - B = " << res << endl; key = 0; }
+
+				else {
+					if (key == 50) { res = B - A; cout << "B - A = " << res << endl;  key = 0; }
+					else { cout << "тут только два выбора! :)" << endl; }
+				}
+			}
+			_getch();
+			break;
+		}
+		case 51:
+		{
+			T x = A * B;
+			cout << "A * B = " << x << endl;
+			_getch();
+			break;
+		}
+		case 52:
+		{
+			T x;
+			cout << "Введите делитель" << endl;
+			cin >> x;
+			while (key != 0) {
+				cout << "Что делим?" << endl << "1. A / x" << endl << "2. B / x" << endl;
+
+
+				key = _getch();
+				if (key == 49) {
+					res = A / x; cout << "A / x = " << res << endl; key = 0;
+				}
+
+				else {
+					if (key == 50) {
+						res = B / x; cout << "B / x = " << res << endl; key = 0;
+					}
+					else { cout << "тут только два выбора! :)" << endl; }
+				}
+			}
+			_getch();
+			break;
+		}
+		case 53:
+		{
+			T x;
+			cout << "Введите множитель" << endl;
+			cin >> x;
+			while (key != 0) {
+				cout << "Что на что умножаем?" << endl << "1. A * x" << endl << "2. x * B" << endl;
+
+
+				key = _getch();
+				if (key == 49) {
+					res = A * x; cout << "A * x = " << res << endl; key = 0;
+				}
+
+				else {
+					if (key == 50) {
+						res = x * B; cout << "x * B = " << res << endl; key = 0;
+					}
+					else { cout << "тут только два выбора! :)" << endl; }
+				}
+			}
+			_getch();
+			break;
+		}
+		case 54:
+		{
+			T x;
+			x = B.square(A, B);
+			cout << "Площадь = " << x << endl;
+			_getch();
+			break;
+		}
+		case 55:
+		{
+			if (A == B) cout << A << "==" << B << endl;
+			else cout << A << "!=" << B << endl;
+			_getch();
+			break;
+		}
+		case 56:
+		{
+
+			cout << "1. Изменить значения вектора 1" << endl << "2. Изменить значения вектора 2" << endl;
+			int y;
+			int x;
+			y = _getch() - 48;
+			if (y == 1)
+			{
+				cout << "Выберите элемент" << A << endl;
+				cin >> x;
+				T temp = A[x];
+				cout << "Введите новое значение" << endl;
+				cin >> A[x];
+				cout << "предыдущее значение: " << temp << "изменено на " << A[x] << endl;
+
+			}
+			else
+			{
+				if (y == 2)
+				{
+					cout << "Выберите элемент" << B << endl;
+					cin >> x;
+					T temp = B[x];
+					cout << "Введите новое значение" << endl;
+					cin >> B[x];
+					cout << "предыдущее значение: " << temp << "изменено на " << B[x] << endl;
+				}
+				else
+				{
+					cout << "У вас только два выбора! Красная или синия таблетка?" << endl;
+					_getch();
+
+				}
+			}
+
+			_getch();
+			break;
+		}
+		case 57:
+		{
+			system("cls");
+			cout << "1. Просмотреть значения вектора 1" << endl << "2. Просмотреть значения вектора 2" << endl;
+			int x;
+			cin >> x;
+			if (x == 1)
+			{
+				cout << A << endl << "какое число вы хотите лицезреть??" << endl;
+				cin >> x;
+				cout << A[x];
+				_getch();
+
+			}
+			else
+			{
+				if (x == 2)
+				{
+					cout << B << endl << "какое число вы хотите лицезреть??" << endl;
+					cin >> x;
+					cout << B[x];
+					_getch();
+
+				}
+				else {
+					cout << "У вас только два выбора! Красная или синия таблетка?" << endl;
+					_getch();
+
+				}
+			}
+		}
+		case 27:
+		{
+			system("cls");
+			break;
+		}
+		}
+	}
+}
+template <class V>
+void Nomain_complex()
+{
+	Vector <complex<V>>A;
+	Vector <complex<V>>B;
+	cout << "Хорошо продолжим.." << endl;
+	int key = 0;
+	while (key != 27)
+	{
+		menu_2();
+		key = _getch();
+		switch (key)
+		{
+		case 49:
+		{
+			Vector <complex<V>> b;
+			B = b;
+			cout << "Ваши рандомные вектора" << endl << "Вектор А: " << A << endl << "Вектор В: " << B << endl;
+
+			//goto random;
+			_getch();
+			key = 27;
+			break;
+		}
+		case 50:
+		{
+			system("cls");
+			cout << "Давайте заполгним наши векторы" << endl << "Введите размерность первого вектора" << endl;
+			int size_one;
+			int size_two;
+			double epsilon;
+			cin >> size_one;
+			cout << "А теперь введем размерность 2 вектора" << endl;
+			cin >> size_two;
+			cout << "Замечательно, а теперь введите точность сравнения" << endl;
+			cin >> epsilon;
+			cout << "Самое время заполнить вектора" << endl;
+			Vector <complex<V>> a(size_one, epsilon);
+			cout << endl << "Остался последний вектор" << endl;
+			Vector <complex<V>> b(size_two, epsilon);
+			A = a;
+			B = b;
+			cout << "Готово, вы справились с этим" << endl << "Вы только поглядите какие они красивые :)" << endl << "Вектор А: " << A << endl << "Вектор В: " << B << endl;
+			//goto Par;
+			_getch();
+			key = 27;
+			break;
+		}
+		case 51:
+		{
+			system("cls");
+			cout << "1. Создать два моих собственных вектора" << endl << "2. Создать два одинаковых и рандомных вектора" << endl;
+			key = _getch();
+			switch (key)
+			{
+			case 49:
+			{
+				int size;
+				complex<V> epsilon;
+				cout << "Давайте заполгним наши векторы" << endl << "Введите размерность векторов" << endl;
+				cin >> size;
+				cout << "Замечательно, а теперь введите точность сравнения" << endl;
+				cin >> epsilon;
+				cout << "Самое время заполнить вектора" << endl;
+				Vector <complex<V>> a(size, epsilon);
+				A = a;
+				B = a;
+				cout << "Готово, вы справились с этим" << endl << "Вы только поглядите какие они красивые :)" << endl << "Вектор А: " << A << endl << "Вектор В: " << B << endl;
+				//goto My_copy;
+				_getch();
+				key = 27;
+				break;
+			}
+			case 50:
+			{
+				B = A;
+				cout << "Ваши рандомные вектора" << endl << "Вектор А: " << A << endl << "Вектор В: " << B << endl;
+
+				//goto random_copy;
+				_getch();
+				key = 27;
+				break;
+			}
+			}
+			_getch();
+			break;
+		}
+		}
+
+	}
+	key = 0;
+	while (key != 27)
+	{
+		menu_3();
+		cout << "Вектор А: " << A << endl << "Вектор В: " << B << endl;
+		key = _getch();
+		Vector <complex<V>>res;
+		switch (key)
+		{
+		case 49:
+		{
+
+			res = A + B;
+			cout << "A + B = " << res << endl;
+			_getch();
+			break;
+		}
+		case 50:
+		{
+			while (key != 0) {
+				cout << "Шо вычитаем?" << endl << "1. A - B" << endl << "2. B - A" << endl;
+
+
+				key = _getch();
+				if (key == 49) { res = A - B; cout << "A - B = " << res << endl; key = 0; }
+
+				else {
+					if (key == 50) { res = B - A; cout << "B - A = " << res << endl;  key = 0; }
+					else { cout << "тут только два выбора! :)" << endl; }
+				}
+			}
+			_getch();
+			break;
+		}
+		case 51:
+		{
+			complex<V> x = A * B;
+			cout << "A * B = " << x.real() << endl;
+			_getch();
+			break;
+		}
+		case 52:
+		{
+			V x;
+			cout << "Введите делитель" << endl;
+			cin >> x;
+			while (key != 0) {
+				cout << "Что делим?" << endl << "1. A / x" << endl << "2. B / x" << endl;
+
+
+				key = _getch();
+				if (key == 49) {
+					res = A / x; cout << "A / x = " << res << endl; key = 0;
+				}
+
+				else {
+					if (key == 50) {
+						res = B / x; cout << "B / x = " << res << endl; key = 0;
+					}
+					else { cout << "тут только два выбора! :)" << endl; }
+				}
+			}
+			_getch();
+			break;
+		}
+		case 53:
+		{
+			V x;
+			cout << "Введите множитель" << endl;
+			cin >> x;
+			while (key != 0) {
+				cout << "Что на что умножаем?" << endl << "1. A * x" << endl << "2. x * B" << endl;
+
+
+				key = _getch();
+				if (key == 49) {
+					res = A * x; cout << "A * x = " << res << endl; key = 0;
+				}
+
+				else {
+					if (key == 50) {
+						res = x * B; cout << "x * B = " << res << endl; key = 0;
+					}
+					else { cout << "тут только два выбора! :)" << endl; }
+				}
+			}
+			_getch();
+			break;
+		}
+		case 54:
+		{
+			V x;
+			x = B.square(A, B);
+			cout << "Площадь = " << x << endl;
+			_getch();
+			break;
+		}
+		case 55:
+		{
+			if (A == B) cout << A << "==" << B << endl;
+			else cout << A << "!=" << B << endl;
+			_getch();
+			break;
+		}
+		case 56:
+		{
+
+			cout << "1. Изменить значения вектора 1" << endl << "2. Изменить значения вектора 2" << endl;
+			int y;
+			int x;
+			y = _getch() - 48;
+			if (y == 1)
+			{
+				cout << "Выберите элемент" << A << endl;
+				cin >> x;
+				complex<V> temp = A[x];
+				cout << "Введите новое значение" << endl;
+				cin >> A[x];
+				cout << "предыдущее значение: " << temp << "изменено на " << A[x] << endl;
+
+			}
+			else
+			{
+				if (y == 2)
+				{
+					cout << "Выберите элемент" << B << endl;
+					cin >> x;
+					complex<V> temp = B[x];
+					cout << "Введите новое значение" << endl;
+					cin >> B[x];
+					cout << "предыдущее значение: " << temp << "изменено на " << B[x] << endl;
+				}
+				else
+				{
+					cout << "У вас только два выбора! Красная или синия таблетка?" << endl;
+					_getch();
+
+				}
+			}
+
+			_getch();
+			break;
+		}
+		case 57:
+		{
+			system("cls");
+			cout << "1. Просмотреть значения вектора 1" << endl << "2. Просмотреть значения вектора 2" << endl;
+			int x;
+			cin >> x;
+			if (x == 1)
+			{
+				cout << A << endl << "какое число вы хотите лицезреть??" << endl;
+				cin >> x;
+				cout << A[x];
+				_getch();
+
+			}
+			else
+			{
+				if (x == 2)
+				{
+					cout << B << endl << "какое число вы хотите лицезреть??" << endl;
+					cin >> x;
+					cout << B[x];
+					_getch();
+
+				}
+				else {
+					cout << "У вас только два выбора! Красная или синия таблетка?" << endl;
+					_getch();
+
+				}
+			}
+		}
+		case 27:
+		{
+			system("cls");
+			break;
+		}
+		}
+	}
+}
+int get_key()
+{
+	int key;
+	key = _getch();
+	system("cls");
+	return key;
+}
+
+int main()
+{
+	int key = 0;
+	while (key != 27)
+	{
+		setlocale(LC_ALL, "Rus");
+		int size;
+
+		try
+		{
+			while (key != 27)
+			{
+				while (key != 27)
+				{
+					menu();
+					key = get_key();
+					switch (key)
+					{
+					case 49:
+					{
+						Nomain<int>();
+						_getch();
+						break;
+					}
+
+					case 50:
+					{
+						Nomain<double>();
+						_getch();
+						break;
+					}
+					case 51:
+					{
+						Nomain<float>();
+						_getch();
+						break;
+					}
+					case 52:
+					{
+						Nomain_complex<double>();
+						_getch();
+						break;
+					}
+					case 53:
+					{
+						Nomain_complex<float>();
+						_getch();
+						break;
+					}
+					}
+
+				}
+			}
+		}
+		catch (invalid_argument err)
+		{
+			cout << "invalid argument: " << err.what() << std::endl;
+			_getch();
+		}
+		catch (out_of_range err)
+		{
+			cout << "Our of range " << err.what() << std::endl;
+			_getch();
+		}
+		catch (logic_error err)
+		{
+			cout << "Logic error: " << err.what() << std::endl;
+			_getch();
+		}
+
+	}
+}
+
+//int main()
+//{
+//	setlocale(LC_ALL, "Rus");
+//	Vector <int> t;
+//	cout << t << endl;
+//
+//	Vector <double> temp(3,1);
+//	cout << temp;
+//
+//	Vector<double> x(temp);
+//	cout << x;
+//
+//	Vector<double> rav;
+//	rav = temp;
+//	cout << rav;
+//	
+//	Vector <double>res = rav + x;
+//	cout << endl <<"a + b" << res;
+//}
